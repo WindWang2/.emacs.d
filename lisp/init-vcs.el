c6ba6f8d166a0fda3770445029baa4de777c52f7
WindWang2 <wangjicheng11@163.com>
2022-10-11 16:35:55 +0800

    add magit-quick-commit-all-and-push

--- lisp/init-vcs.el
+++ lisp/init-vcs.el
@@ -41,7 +41,24 @@
   :config
   (when sys/win32p
     (setenv "GIT_ASKPASS" "git-gui--askpass"))
-
+  (defun magit-quick-git-add-commit-push ()
+    "Simple commit current git project and push to its upstream."
+    (interactive)
+    (when (and buffer-file-name
+               (buffer-modified-p))
+      (save-buffer))                   ;; save it first if modified.
+    (magit-diff-unstaged)
+    (when (yes-or-no-p "Do you really want to commit everything?")
+      (magit-stage-modified)
+      (magit-diff-staged)
+      (let ((msg (read-string "Commit Message: ")))
+        (when (= 0 (length msg))
+          (setq msg (format-time-string "commit by magit in emacs@%Y-%m-%d %H:%M:%S"
+                                        (current-time))))
+        (magit-call-git "commit" "-m" msg)
+        (magit-push-current-to-upstream nil)
+        (message "now do async push to %s" (magit-get "remote" "origin" "url"))))
+    (magit-mode-bury-buffer))
   ;; Exterminate Magit buffers
   (with-no-warnings
     (defun my-magit-kill-buffers (&rest _)
