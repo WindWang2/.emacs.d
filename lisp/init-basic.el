e36cb9d5176b7b6f81ebec29fe4337deedd2a8da
WindWang2 <wangjicheng11@163.com>
2022-10-28 17:26:04 +0800

    Remote `set-charset-priority` ref: emacs-china

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -95,8 +95,8 @@
 
 ;; Encoding
 ;; UTF-8 as the default coding system
-(when (fboundp 'set-charset-priority)
-  (set-charset-priority 'unicode))
+;; (when (fboundp 'set-charset-priority)
+;;   (set-charset-priority 'unicode))
 ;; Explicitly set the prefered coding systems to avoid annoying prompt
 ;; from emacs (especially on Microsoft Windows)
 (prefer-coding-system 'utf-8)
