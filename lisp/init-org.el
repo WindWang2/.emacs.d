ae77454c36343a4d3403402301933058d7bca3b9
WindWang2 <wangjicheng11@163.com>
2022-10-29 21:09:35 +0800

    fix latex preview bug in Windows

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1061,7 +1061,59 @@ the \"file\" field is empty, return the empty string."
 (setq org-latex-packages-alist '(("" "minted")))
 (setq org-latex-minted-options '(("breaklines" "true")
                                  ("breakanywhere" "true")))
+
+;; for latex preview process  ----------------------------------------------------------
+;; ref: https://emacs-china.org/t/org-latex-preview/22288/8
+(setq org-preview-latex-process-alist
+      '((dvisvgm :programs
+          ("latex" "dvisvgm")
+          :description "dvi > svg" :message "you need to install the programs: latex and dvisvgm." :image-input-type "dvi" :image-output-type "svg" :image-size-adjust
+          (1.7 . 1.5)
+          :latex-compiler
+          ("latex -interaction nonstopmode -shell-escape -output-directory %o %f")
+          :image-converter
+          ("dvisvgm %f -e -n -b 1 -c %S -o %O"))))
 (setq org-preview-latex-default-process 'dvisvgm)
+(defun my/org--latex-header-preview (orig &rest args)
+  "Setup dedicated `org-format-latex-header' to `my/org--match-text-baseline-ascent'."
+  (let ((org-format-latex-header
+         "\\documentclass[preview]{standalone}
+\\usepackage[usenames]{color}
+[PACKAGES]
+[DEFAULT-PACKAGES]"))
+    (apply orig args)))
+
+(defun my/org--match-text-baseline-ascent (imagefile)
+  "Set `:ascent' to match the text baseline of an image to the surrounding text.
+Compute `ascent' with the data collected in IMAGEFILE."
+  (advice-add 'org-create-formula-image :around #'my/org--latex-header-preview)
+  (let* ((viewbox (split-string
+                   (xml-get-attribute (car (xml-parse-file imagefile)) 'viewBox)))
+         (min-y (string-to-number (nth 1 viewbox)))
+         (height (string-to-number (nth 3 viewbox)))
+         (ascent (round (* -100 (/ min-y height)))))
+    (if (or (< ascent 0) (> ascent 100))
+        'center
+      ascent)))
+
+(defun org--make-preview-overlay (beg end image &optional imagetype)
+  "Build an overlay between BEG and END using IMAGE file.
+Argument IMAGETYPE is the extension of the displayed image,
+as a string.  It defaults to \"png\"."
+  (let ((ov (make-overlay beg end))
+	    (imagetype (or (intern imagetype) 'png)))
+    (let ((ascent (my/org--match-text-baseline-ascent image)))
+      (overlay-put ov 'org-overlay-type 'org-latex-overlay)
+      (overlay-put ov 'evaporate t)
+      (overlay-put ov
+		           'modification-hooksn
+		           (list (lambda (o _flag _beg _end &optional _l)
+			               (delete-overlay o))))
+      (overlay-put ov
+		           'display
+		           (list 'image :type imagetype :file image :ascent ascent)))))
+
+
 
 
 (provide 'init-org)
