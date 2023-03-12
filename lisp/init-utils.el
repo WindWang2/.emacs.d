cbf60a6d151e07d1f3c182a8cb0f1807aa16a422
WindWang2 <wangjicheng11@163.com>
2023-03-12 22:40:06 +0800

    fix bug

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -831,7 +831,7 @@
    consult-bookmark consult-recent-file consult-xref
    consult--source-bookmark consult--source-recent-file
    consult--source-project-recent-file
-   :preview-key (kbd "M-."))
+   :preview-key "M-.")
   (setq consult-narrow-key "<") ;; (kbd "C-+")
   )
 (use-package marginalia
