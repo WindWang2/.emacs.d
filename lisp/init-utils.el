e9ad90cf4873aa82637c65f344b7b285d030b5f0
WindWang2 <wangjicheng11@163.com>
2023-04-19 23:45:53 +0800

    remove the sis

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -494,36 +494,36 @@
   (global-set-key (kbd "C-c k") 'evilnc-copy-and-comment-lines)
   (global-set-key (kbd "C-c m") 'evilnc-comment-or-uncomment-paragraphs))
 
-(use-package sis
-  ;; :hook
-  ;; enable the /follow context/ and /inline region/ mode for specific buffers
-  ;; (((text-mode prog-mode) . sis-context-mode)
-  ;;  ((text-mode prog-mode) . sis-inline-mode))
-  :init
-  :bind ("C-\\" . sis-switch)
-  :config
-  (when sys/win32p
-    (sis-ism-lazyman-config nil t 'w32))
-  (when sys/macp
-    (sis-ism-lazyman-config
+;; (use-package sis
+;;   ;; :hook
+;;   ;; enable the /follow context/ and /inline region/ mode for specific buffers
+;;   ;; (((text-mode prog-mode) . sis-context-mode)
+;;   ;;  ((text-mode prog-mode) . sis-inline-mode))
+;;   :init
+;;   :bind ("C-\\" . sis-switch)
+;;   :config
+;;   (when sys/win32p
+;;     (sis-ism-lazyman-config nil t 'w32))
+;;   (when sys/macp
+;;     (sis-ism-lazyman-config
 
-     ;; English input source may be: "ABC", "US" or another one.
-     ;; "com.apple.keylayout.ABC"
-     "com.apple.keylayout.US"
+;;      ;; English input source may be: "ABC", "US" or another one.
+;;      ;; "com.apple.keylayout.ABC"
+;;      "com.apple.keylayout.US"
 
-     ;; Other language input source: "rime", "sogou" or another one.
-     ;; "im.rime.inputmethod.Squirrel.Rime"
-     "com.sogou.inputmethod.sogou.pinyin"))
-  ;; enable the /cursor color/ mode
-  (sis-global-cursor-color-mode t)
-  ;; enable the /respect/ mode
-  (sis-global-respect-mode t)
-  ;; enable the /context/ mode for all buffers
-  (sis-global-context-mode t)
-  ;; enable the /inline english/ mode for all buffers
-  (sis-global-inline-mode t)
-  (setq-default sis-inline-tighten-head-rule 0)
-  )
+;;      ;; Other language input source: "rime", "sogou" or another one.
+;;      ;; "im.rime.inputmethod.Squirrel.Rime"
+;;      "com.sogou.inputmethod.sogou.pinyin"))
+;;   ;; enable the /cursor color/ mode
+;;   (sis-global-cursor-color-mode t)
+;;   ;; enable the /respect/ mode
+;;   (sis-global-respect-mode t)
+;;   ;; enable the /context/ mode for all buffers
+;;   (sis-global-context-mode t)
+;;   ;; enable the /inline english/ mode for all buffers
+;;   (sis-global-inline-mode t)
+;;   (setq-default sis-inline-tighten-head-rule 0)
+;;   )
 
 ;; Siri shortcusts: for translating
 ;; -ref: https://emacs-china.org/t/macos/23234, https://emacs-china.org/t/emacs-macos/23268/6
